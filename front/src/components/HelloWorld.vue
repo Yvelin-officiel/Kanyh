7a309584e0bb97a03a6891b3dfaad8f3cd79c115
Hippot <hippot02@gmail.com>
2025-10-08 11:30:18 +0200

    Ajout de tailwind

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,43 +1,8 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="bg-blue-500 text-white p-4 rounded-lg shadow-md justify-center items-center flex flex-col min-h-screen">
+    <h1 class="text-2xl font-bold mb-2">Hello, World!</h1>
+    <p class="text-lg">Welcome to your Vue.js application with Tailwind CSS!</p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
