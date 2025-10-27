52f151d6213743d374b45f581bb649228c867b48
IceK37 <45316097+IceK37@users.noreply.github.com>
2025-10-27 10:43:45 +0100

    specialite crud + test

--- back/src/main/java/com/data/kanyh/repository/SpecialiteRepository.java
+++ back/src/main/java/com/data/kanyh/repository/SpecialiteRepository.java
@@ -1,6 +1,6 @@
-package repository;
+package com.data.kanyh.repository;
 
-import model.Specialite;
+import com.data.kanyh.model.Specialite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
