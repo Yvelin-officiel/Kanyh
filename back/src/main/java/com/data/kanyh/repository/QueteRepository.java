fb20e0149ea7582e5f210003af849b13a074fb55
NRIVALANT <nicolas.rivalant@gmail.com>
2025-10-27 10:22:21 +0100

    [FEAT] : Modification repository

--- back/src/main/java/com/data/kanyh/repository/QueteRepository.java
+++ back/src/main/java/com/data/kanyh/repository/QueteRepository.java
@@ -7,9 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QueteRepository extends JpaRepository<Quete, Long> {
-    // Méthode de base héritées : findAll(), findById(), save(), deleteById()
-
-    //Méthode custom possibles :
-    List<Quete> findByStatut(StatutQuete statut);
-    List<Quete> findByEquipeId(Long id);
 }
